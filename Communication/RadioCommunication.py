cd4e6b645fb1bc5fb797d79d5d64399320420a60
clement-remillieux <57375319+clement-remillieux@users.noreply.github.com>
2021-10-22 15:19:11 +0100

    Add files via upload

--- Communication/RadioCommunication.py
+++ Communication/RadioCommunication.py
@@ -11,41 +11,35 @@ class RadioCommuncation:
 		self.isMaster = self.checkMaster()
 		self.requestMessage = ["wantToSpeak"]
 		self.receivedMessage = []
-		self.dictCommande = {"startByte": 0x00,
+		self.dictCommande = {"startByte": 0x00,                  
 		 					 "stopByte": 0xFF,
 							 "wantToSpeak" : 0x10,
 							 "PermissionToSpeak" : 0x11,
-							}
+                             "TurnLeft" : 0x12,
+                             "TurnRigth" : 0x13,
+                             "Go" : 0x14,
+                             "Back" : 0x15,
+							}# hexa : 0 , 255 , 16 , 17 
 
 	def checkMaster(self):
-		if self.Name = 1:
+		if self.Name == 1:
 			return True
 		return False
 
 	def readAll(self, listName):
 		while True:
-			node_id, payload = self.Radio.readAll()
-			if not self.isReceiving:
-				if payload != self.dictCommande["startByte"]:
-					pass
-				if payload == self.dictCommande["startByte"]:
-					self.isReceiving = True
-					self.receivedMessage.append(payload)
-
-			elif self.isReceiving:
-				if payload == self.dictCommande["stopByte"]:
-					self.receivedMessage.append(payload)
-					self.isReceiving = False
-					data = [node_id, payload]
-					return data
-				else:
-					self.receivedMessage.append(payload)
+			payload = self.Radio.readAll()
+			self.decodeData(payload)
 		
 	def concatenateMessage(self):
 
 	def write(self, listName, data):
-		for byte in data:
-			self.Radio.write(listName,byte)
+        data = codeData(self,data)
+		self.Radio.write(listName,data)
+		
+	def writeAll(self, listName, data):
+        data = codeData(self,data)
+		self.Radio.writeAll(data)
 
 	def decodeData(self,data):
 		node_id, message = self.convertData(data)
@@ -58,35 +52,54 @@ class RadioCommuncation:
 			if message[0] == self.dictCommande["PermissionToSpeak"]:
 				if message[1] == self.Name:
 					self.asPermissionToSpeak = True
-					self.asPermissionToSpeak = False
+					self.isSilent = False
 				else:
 					self.asPermissionToSpeak = False
 					self.isSilent = True
-
-	def convertData(self,payload):
+            else:
+                self.executeCmd(message)
+                
+    def executeCmd(message):
+        
+        if message == self.dictCommande["TurnLeft"]:
+            print("Turn Left")
+        if message == self.dictCommande["TurnRigth"]:
+            print("Turn Rigth")
+        if message == self.dictCommande["Go"]:
+            print("Go")
+        if message == self.dictCommande["Back]:
+            print("Back")
+            
+	def convertData(self,data):
 		node_id = data[0]
-		payload = data[1]
-		message = payload[1:-1]
+		message = data[2]
+		node_id = data[1]
 		return node_id, message
 
 	def wantToSpeak(self,data):
-		data = self.codeData(self.dictCommande[self.requestMessage])
+		data = self.codeData(self.dictCommande["wantToSpeak"])
 		self.write([0],data)
 
 	def send(self,data):
-		if self.isSilent:
-			return False
-		if self.isIdle :
-			data = self.codeData(data)
-			self.wantToSpeak()
-			data = self.readAll()
-			self.decodeData(data)
-			if self.asPermissionToSpeak:
-				self.write([0],self.message)
-				return True
+        if self.isMaster:
+            self.writeAll(data)
+            return True
+        else:
+            if self.isSilent:
+                return False
+            if self.isIdle :
+                data = self.codeData(data)
+                self.wantToSpeak()
+                response = self.readAll()
+                self.decodeData(response)
+                if self.asPermissionToSpeak:
+                    self.write([0],data)
+                    return True
+        return False
+        
 
 	def codeData(self,data):
-		data = self.dictCommande["startByte"] + data + self.dictCommande["stopByte"]
+		data = self.dictCommande["startByte"] + str(self.Name) + data + self.dictCommande["stopByte"]
 		return data
 
 	def listen(self):
@@ -94,4 +107,10 @@ class RadioCommuncation:
 			data = self.readAll()
 			self.decodeData(data)
 
+rc1 = RadioCommuncation(1)
+while True:
+    rc1.listen()
 
+rc2 = RadioCommuncation(2)
+while True:
+    rc2.send(self.dictCommande["Go"])
