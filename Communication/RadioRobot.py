44684d132572ffb6c41c73a3e7fa8c97eb72c2a8
clement-remillieux <57375319+clement-remillieux@users.noreply.github.com>
2021-10-22 14:59:12 +0100

    Add files via upload

--- Communication/RadioRobot.py
+++ Communication/RadioRobot.py
@@ -2,104 +2,107 @@ from circuitpython_nrf24l01.rf24 import RF24
 import board
 import digitalio
 import copy
+import struct
+import time
 
 class RadioRobot:
 
-	def __init__(self, Name):
+    def __init__(self, Name):
 
-		self.dictAllAddress = {1:b"1Node", 2:b"2Node"}
-		self.initSpi()
-		self.Name, self.dictAddress, self.ownAddress = self.setIdentity(Name)
+        self.dictAllAddress = {1:b"1Node", 2:b"2Node"}
+        self.initSpi()
+        self.Name = Name
+        self.ownAddress = ""
+        self.dictAddress = self.setIdentity(Name)
 
+    def initSpi(self):
+        # change these (digital output) pins accordingly
+        ce = digitalio.DigitalInOut(board.D4)
+        csn = digitalio.DigitalInOut(board.D5)
 
-	def initSpi(self):
-		# change these (digital output) pins accordingly
-		ce = digitalio.DigitalInOut(board.D4)
-		csn = digitalio.DigitalInOut(board.D5)
+        # using board.SPI() automatically selects the MCU's
+        # available SPI pins, board.SCK, board.MOSI, board.MISO
+        spi = board.SPI()  # init spi bus object
 
-		# using board.SPI() automatically selects the MCU's
-		# available SPI pins, board.SCK, board.MOSI, board.MISO
-		spi = board.SPI()  # init spi bus object
+        # we'll be using the dynamic payload size feature (enabled by default)
+        # initialize the nRF24L01 on the spi bus object
+        self.nrf = RF24(spi, csn, ce)
 
-		# we'll be using the dynamic payload size feature (enabled by default)
-		# initialize the nRF24L01 on the spi bus object
-		nrf = RF24(spi, csn, ce)
+        # set the Power Amplifier level to -12 dBm since this test example is
+        # usually run with nRF24L01 transceivers in close proximity
+        self.nrf.pa_level = -18
 
-		# set the Power Amplifier level to -12 dBm since this test example is
-		# usually run with nRF24L01 transceivers in close proximity
-		nrf.pa_level = -12
+    def setIdentity(self, Name):
+            dictAddressTx = copy.deepcopy(self.dictAllAddress)
+            dictAddressTx.pop(Name)
+            self.ownAddress = self.dictAllAddress[Name]
+            return dictAddressTx
 
-	def setIdentity(self, Name):
-			dictAddressTx = copy.deepcopy(self.dictAllAddress)
-			dictAddressTx.pop(Name)
-			dictAddressRx["me"] = self.dictAllAddress[Name]
-			return dictAddressTx, dictAddressRx
-
-	def openListenChanel(self, dictAddress):
-		for name in dictAddress.keys():
-        	nrf.open_rx_pipe(name, dictAddress[name])
-        nrf.listen = True 
+    def openListenChanel(self, dictAddress):
+        for name in dictAddress.keys():
+            self.nrf.open_rx_pipe(name, dictAddress[name])
+        self.nrf.listen = True
+        print("Channel for listening from:", list(dictAddress.keys()))
 
     def listenChanel(self):
-    	while True:
-	    	while not nrf.fifo(False, True):  # keep RX FIFO empty for reception
-	            # show the pipe number that received the payload
-	            # NOTE read() clears the pipe number and payload length data
-	            print("Received", nrf.any(), "on pipe", nrf.pipe, end=" ")
-	            node_id, payload_id = struct.unpack("<ii", nrf.read())
-	            print("from node {}. PayloadID: {}".format(node_id, payload_id))
-	        return node_id, payload_id
+        print("listening..")
+        while True:
+            if self.nrf.available():  # keep RX FIFO empty for reception
+                payload_size, pipe_number = (self.nrf.any(), self.nrf.pipe)
+                print("Received", self.nrf.any(), "on pipe", self.nrf.pipe, ":")
+                receiveData = self.nrf.read()
+                payload = []
+                for char in receiveData:
+                    payload.append(chr(char))
+                print(payload)
+                return payload
 
     def openSpeakChanel(self, dictAddress):
-    	nrf.listen = False
-		for name in dictAddress.keys():
-        	nrf.open_tx_pipe(name, dictAddress[name])
+        
+        for name in dictAddress.keys():
+            self.nrf.open_tx_pipe(self.ownAddress)
+        print("Channel for writing from:", list(dictAddress.keys()))
+        self.nrf.listen = False
          
     def speakChanel(self, data, dictAddress):
-    	for name in dictAddress.keys():
-	    	payload = struct.pack("<ii", dictAddress[name], data)
-	        # show something to see it isn't frozen
-	        start_timer = time.monotonic_ns()
-	        report = nrf.send(payload)
-	        end_timer = time.monotonic_ns()
-	        # show something to see it isn't frozen
-	        if report:
-	            print(
-	                "Transmission of payloadID {} as node {} successfull! "
-	                "Transmission time: {} us".format(
-	                    counter, name, (end_timer - start_timer) / 1000
-	                )
-	            )
-	        else:
-	            print("Transmission failed or timed out")
+        for name in dictAddress.keys():
+            payload = data.encode("ascii")
+            report = self.nrf.send(payload)
+            if report:
+                print("Transmission  successfull! ")
+            else:
+                print("Transmission failed or timed out")
 
     def read(self, listName):
-    	dictAddressRead = {}
-    	for name in listName
-    		dictAddressRead[name] = self.dictAddress[name]
-    	self.openListenChanel(dictAddressRead)
-    	node_id, payload = self.listenChanel()
-    	return node_id, payload
+        dictAddressRead = {}
+        for name in listName:
+            dictAddressRead[name] = self.dictAddress[name]
+        self.openListenChanel(dictAddressRead)
+        payload = self.listenChanel()
+        return payload
 
     def readAll(self):
-    	self.openListenChanel(self.dictAddress)
-    	node_id, payload = self.listenChanel()
-    	return node_id, payload
+        print("start listening")
+        self.openListenChanel(self.dictAddress)
+        payload = self.listenChanel()
+        return payload
 
-   	def write(self, listName, data):
-   		dictAddressWrite = {}
-    	for name in listName
-    		dictAddressWrite[name] = self.dictAddress[name]
-    	self.openSpeakChanel(dictAddressWrite)
-    	self.speakChanel(data,dictAddressWrite)
+    def write(self, listName, data):
+        dictAddressWrite = {}
+        for name in listName:
+            dictAddressWrite[name] = self.dictAddress[name]
+        self.openSpeakChanel(dictAddressWrite)
+        self.speakChanel(data,dictAddressWrite)
 
     def writeAll(self, data):
-    	self.openSpeakChanel(self.dictAddress)
-    	self.speakChanel(data,self.dictAddress)
+        print("Start writing")
+        self.openSpeakChanel(self.dictAddress)
+        self.speakChanel(data,self.dictAddress)
 
 
-RadioRobot1 = RadioRobot(1)
-RadioRobot1.read([2])
+# RadioRobot1 = RadioRobot(1)
+# while True:
+#      RadioRobot1.readAll()
 
-RadioRobot2 = RadioRobot(2)
-RadioRobot2.write([1],"salut")
+#RadioRobot2 = RadioRobot(2)
+"RadioRobot2.writeAll("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
